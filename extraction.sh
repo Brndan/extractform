db1b4586e4300b35b74450ac4d696deebdc0792e
Brendan Chabannes <brendan.chabannes@hey.com>
2021-11-09 14:51:58 +0100

    initial release

---
+++ extraction.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Pour chaque département, produire une liste des personnels
+# avec le champ correspondant à l'heure de l'émargement
+# ou un champ vide si le personnel n'a pas émargé
+
+
+if [ -d export ]; then
+    rm -r export
+fi
+
+mkdir -p export/{CSV,ODS}
+
+# -z permet que le délimiteur soit \0 et pas \n
+sed -i -z -e 's/\"Par e-mail\nPar téléphone\"/Par e-mail et par téléphone/g' fichiers.csv
+
+# Corps DiscPLP DiscCertif Dpt-affectation Dpt-vis academie-visee mailOUtel Nom Prénom mail tel
+
+while read -r line ; do 
+    
+    (
+        echo 'Prénom,Nom,Mail,Téléphone,Corps,Département d’affectation,Département visé,Académie visée,Discipline si LP,Discipline si certifié⋅e agrégé⋅e' > ./export/CSV/"${line}.csv"
+
+        awk -F',' -v dpt="${line}" '$4==dpt {printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",$9,$8,$10,$11,$1,$4,$5,$6,$2,$3 }' "$1"  >> ./export/CSV/"${line}.csv"
+    ) &
+
+done < departements
+wait
+
+
+(
+cd export/CSV || exit
+nom=""
+    for i in *.csv ; do
+        (
+            nom="${i%.*}"
+            #nom=$(awk -F"," -v code="$code" '$2 ~ code {print $6}' ../../departements)
+            #mv "$i"  "$nom".csv
+            
+            # Libreoffice 
+            #unoconv -i FilterOptions=44,34,76 -f ods "$nom".csv
+            
+            # Gnumeric
+            ssconvert "${i}" "$nom".ods
+        ) &
+    done
+    wait
+    
+
+    mv ./*.ods ../ODS/
+)
