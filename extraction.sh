371524db64b0cbf94cf7ba626e7dadd454d3a462
Brendan Chabannes <brendan.chabannes@hey.com>
2021-11-09 19:10:48 +0100

    bugfix

--- extraction.sh
+++ extraction.sh
@@ -13,7 +13,7 @@ fi
 mkdir -p export/{CSV,ODS}
 
 # -z permet que le délimiteur soit \0 et pas \n
-sed -i -z -e 's/\"Par e-mail\nPar téléphone\"/Par e-mail et par téléphone/g' "${1}"
+sed -z -e 's/\"Par e-mail\nPar téléphone\"/Par e-mail et par téléphone/g' "${1}" |  tr -d '\"' > clean.csv
 
 # Corps DiscPLP DiscCertif Dpt-affectation Dpt-vis academie-visee mailOUtel Nom Prénom mail tel
 
@@ -22,7 +22,7 @@ while read -r line ; do
     (
         echo 'Prénom,Nom,Mail,Téléphone,Corps,Département d’affectation,Département visé,Académie visée,Discipline si LP,Discipline si certifié⋅e agrégé⋅e' > ./export/CSV/"${line}.csv"
 
-        awk -F',' -v dpt="${line}" '$4==dpt {printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",$9,$8,$10,$11,$1,$4,$5,$6,$2,$3 }' "$1"  >> ./export/CSV/"${line}.csv"
+        awk -F',' -v dpt="${line}" '$4==dpt {printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",$9,$8,$10,$11,$1,$4,$5,$6,$2,$3 }' clean.csv  >> ./export/CSV/"${line}.csv"
     ) &
 
 done < departements
@@ -48,4 +48,4 @@ nom=""
     mv ./*.ods ../ODS/
 )
 
-awk -F',' '{print $4}' "${1}" | tr -d '\"' | sort | uniq 
+awk -F',' '{print $4}' clean.csv | tr -d '\"' | sort | uniq 
